d9b267535270c72ced4ad562ccd8f5a36ff15f04
Jan Jakub Czechowicz <68144860+jczechowicz@users.noreply.github.com>
2023-03-21 18:51:13 +0100

    fix: made necessary improvements for game to run

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
+let playerScore = 0
+let computerScore = 0
+const playerChoice = document.querySelectorAll('button')
+const computerScore_span = document.getElementById('computer-score')
+const playerScore_span = document.getElementById('human-score')
+
 function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * choices.length);
+  const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  }
-
-  if (playerSelection === 'rock') {
-    if (computerSelection === 'paper') {
-      return "You Lose! Paper beats Rock";
-    } else {
-      return "You Win! Rock beats Scissors";
-    }
-  }
-
-  if (playerSelection === 'paper') {
-    if (computerSelection === 'scissors') {
-      return "You Lose! Scissors beats Paper";
-    } else {
-      return "You Win! Paper beats Rock";
-    }
-  }
-
-  if (playerSelection === 'scissors') {
-    if (computerSelection === 'rock') {
-      return "You Lose! Rock beats Scissors";
-    } else {
-      return "You Win! Scissors beats Paper";
-    }
-  }
+function disableOptions() {
+  playerChoice.forEach(elem => {
+      elem.disabled = true
+  })
 }
 
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice()
+  computerScore_span.innerHTML = computerScore
+  playerScore_span.innerHTML = playerScore
+  let result = ""
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  playRound();
+  if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        playerScore++
+        result = ("Outsmarting computer... wow. It's a win!")
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, Paper, or Scissors?");
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
-    console.log(result);
+        if (playerScore == 5) {
+            result += '<br>Computers defeated. You won the game, champ! Reload the page to play again'
+            disableOptions()
+        }
+    } else if (playerSelection == computerSelection) {
+        result = ("Nice try, but it's a tie! You both chose " + playerSelection)
+    } else {
+        computerScore++
+        result = ("You lose. Reject humanity, enhance computersity!")
 
-    if (result.includes("Win")) {
-      playerScore++;
-    } else if (result.includes("Lose")) {
-      computerScore++;
+          if (computerScore == 5) {
+            result += '<br>Hahah, stupid human! I won the game! Reload the page to play again'
+            disableOptions()
+        }
     }
-  }
-
-  if (playerScore > computerScore) {
-    console.log(`You won the game! Your score was ${playerScore} and the computer's score was ${computerScore}.`);
-  } else if (playerScore < computerScore) {
-    console.log(`You lost the game! Your score was ${playerScore} and the computer's score was ${computerScore}.`);
-  } else {
-    console.log(`The game was a tie! Your score was ${playerScore} and the computer's score was ${computerScore}.`);
-  }
+    document.getElementById('result').innerHTML = result
+    return
 }
+
+playerChoice.forEach(choice =>{
+  choice.addEventListener('click', function(){
+    playRound(choice.value)
+  })
+})
